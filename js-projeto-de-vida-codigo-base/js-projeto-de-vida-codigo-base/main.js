f5e8255a9352d7e6ee0e778898d09580ddf24fe7
Mendonça <132940434+mandinha007@users.noreply.github.com>
2025-06-17 13:36:20 -0300

    Add files via upload

--- js-projeto-de-vida-codigo-base/js-projeto-de-vida-codigo-base/main.js
+++ js-projeto-de-vida-codigo-base/js-projeto-de-vida-codigo-base/main.js
@@ -16,15 +16,23 @@ console.log(i);
 }
 
 const contadores = document.querySelectorAll(".contador");
-let tempoAtual = new Date ();
+
 const tempoObjetivo1 = new Date("2025-12-12T00:00:00") - tempoAtual;
-contadores[0].textContent = tempoObjetivo1;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 const tempoObjetivo2 = new Date("2025-12-31T00:00:00") - tempoAtual;
-contadores[1].textContent = tempoObjetivo2;
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
 const tempoObjetivo3 = new Date("2030-12-31T00:00:00") - tempoAtual;
-contadores[2].textContent = tempoObjetivo3;
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
 const tempoObjetivo4 = new Date("2035-05-05T00:00:00") - tempoAtual;
-contadores[3].textContent = tempoObjetivo4;
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
+
+function calculaTempo(tempoObjetivo){
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = tempoFinal/1000;
+return segundos;
+
+}
 
 
 
