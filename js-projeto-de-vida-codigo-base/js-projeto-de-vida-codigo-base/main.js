fe8a7aac98c306473556a01a0de8c51a9815583e
Mendonça <132940434+mandinha007@users.noreply.github.com>
2025-08-12 16:31:30 -0300

    Add files via upload

--- js-projeto-de-vida-codigo-base/js-projeto-de-vida-codigo-base/main.js
+++ js-projeto-de-vida-codigo-base/js-projeto-de-vida-codigo-base/main.js
@@ -19,12 +19,23 @@ const contadores = document.querySelectorAll(".contador");
 
 const tempoObjetivo1 = new Date("2025-12-12T00:00:00");
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
-const tempoObjetivo2 = new Date("2025-12-31T00:00:00");
+const tempoObjetivo2 = new Date("2026-01-13T00:00:00");
 contadores[1].textContent = calculaTempo(tempoObjetivo2);
 const tempoObjetivo3 = new Date("2030-12-31T00:00:00");
 contadores[2].textContent = calculaTempo(tempoObjetivo3);
 const tempoObjetivo4 = new Date("2035-05-26T00:00:00");
 contadores[3].textContent = calculaTempo(tempoObjetivo4);
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
+
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
 
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
